bd447c55c7469f8e32856300c97b72f26ec6900f
tyrion <devnull@localhost>
2010-09-18 15:48:40 +0200

    Added support for jQuery UI Autocomplete. The old jQuery Autocomplete Plugin is still usable with the "bassistance_autocomplete" function in the jquery_autocomplete.js.

--- autocomplete/media/js/jquery_autocomplete.js
+++ autocomplete/media/js/jquery_autocomplete.js
@@ -1,4 +1,4 @@
-function jquery_autocomplete(name, ac_url, force_selection) {
+function bassistance_autocomplete(name, ac_url, force_selection) {
     $(document).ready(function () {
         var input = $('#id_' + name);
         var hidden_input = $('#id_hidden_' + name);
@@ -33,5 +33,68 @@ function jquery_autocomplete(name, ac_url, force_selection) {
         });
     });
 }
+
+
+function jquery_autocomplete(name, ac_url, force_selection) {
+
+    this.name = name;
+    this.ac_url = ac_url;
+    this.force_selection = force_selection;
+
+    this.source = function (request, response) {
+        function success(data) {
+            var parsed = [];
+            for (var i in data) {
+                parsed[parsed.length] = {
+                    id: data[i][0],
+                    value: data[i][1],
+                    label: data[i][1],
+                };
+            }
+            response(parsed);
+        };
+        $.ajax({
+            url: this.ac_url,
+            dataType: "json",
+            data: {q: request.term},
+            success: success
+        });
+    };
+
+    this.select = function (event, ui) {
+        // set the hidden input field.
+        this.last_item = ui.item;
+        this.hidden_input.val(ui.item.id);
+    };
+
+    this.close = function (event, ui) {
+        alert(ui.toSource());  
+    };
+
+    this.setup = function () {
+        this.input = $("#id_" + this.name);
+        this.hidden_input = $("#id_hidden_" + this.name);
+        this.last_item = {};
+        this.input.autocomplete({
+            // minLength: 2,
+            source: jQuery.proxy(this.source, this),
+            select: jQuery.proxy(this.select, this),
+        });
+        this.input.closest("form").submit(jQuery.proxy(function () {
+            if ((!this.input.val()) || (this.hidden_input.val() != this.input.val()
+                && !this.force_selection)) {
+                this.hidden_input.val(this.input.val());
+            }
+        }, this));
+        if (this.force_selection) {
+            this.input.focusout(jQuery.proxy(function (event) {
+                if (this.input.val() != this.last_item.value)
+                    this.input.val("");
+            }, this));
+        }
+    };
+
+    $(document).ready(jQuery.proxy(this.setup, this));
+};
 
 autocomplete = jquery_autocomplete;
